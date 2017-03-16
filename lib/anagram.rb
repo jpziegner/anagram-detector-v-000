a575acc86d6e9237133a401ff06cab55da4f023a
John Ziegner <jziegner23@gmail.com>
2017-03-16 22:40:21 +0000

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_match = []
+    array.select do |words|
+      characters = words.split("").sort
+      second_word = word.split("").sort
+      if characters == second_word
+        word_match << words
+      end
+    end
+  word_match
+  end
+end
